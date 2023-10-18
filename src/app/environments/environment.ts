122187d64cac1791a43e4c408eaed788788cad5e
Luis-Cici <thukhantsi771@gmail.com>
2023-10-18 13:44:38 +0630

    first commit

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    firebaseConfig : {
+    apiKey: "AIzaSyBPRzxtw1nMbMxFbDL7V2MuuvPs3tcD5ec",
+    authDomain: "social-media-5ab0e.firebaseapp.com",
+    projectId: "social-media-5ab0e",
+    storageBucket: "social-media-5ab0e.appspot.com",
+    messagingSenderId: "816335286994",
+    appId: "1:816335286994:web:c535db1340bb1d8ba0f30b",
+    measurementId: "G-3NFQZRJJC0"
+  }
+}
